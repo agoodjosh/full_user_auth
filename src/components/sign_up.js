407f0e6094c9c3b1c38205e8f0ccdd4c5e4a2cde
jauxsh <joshua.jih.chang@gmail.com>
2018-01-19 17:32:17 -0800

    started to connect to server

--- src/components/sign_up.js
+++ src/components/sign_up.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { renderInput } from '../helpers';
+import { signUp } from '../actions';
 
 class SignUp extends Component {
 
@@ -8,6 +10,8 @@ class SignUp extends Component {
 
     handleSignUp(vals) {
         console.log('Vals', vals);
+
+        this.props.signUp(vals);
     }
 
     render() {
@@ -58,4 +62,4 @@ SignUp = reduxForm({
     validate: validate
 })(SignUp);
 
-export default SignUp;
+export default connect(null, { signUp })(SignUp);
